1bbd090ed623ebb783845f084fffde2673b028d8
Xuân Hiển <hienphannguyenxuan@gmail.com>
2026-01-27 15:47:59 +0700

    add volume

--- src/components/component/Header.tsx
+++ src/components/component/Header.tsx
@@ -1,22 +1,31 @@
-
+import  { useRef } from "react";
 
 const Header = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const play = () => {
+    audioRef.current?.play();
+  };
+
   return (
-    <div className="w-full flex items-center justify-between p-4 bg-white shadow">
+    <div className="p-4 bg-white shadow flex justify-between">
+      <h1>Quiz App</h1>
 
-  <h1 className="text-xl font-semibold">Quiz App</h1>
+      <button 
+        onClick={play}
+        className="bg-blue-500 text-white px-3 py-1 rounded"
+      >
+        Bật nhạc
+      </button>
 
-  <audio 
-    src="/public/kkk.mp3"
-    autoPlay
-    loop
-    hidden
-  
-  />
+      <audio 
+        ref={audioRef}
+        src="/audio/kkk.mp3"
+        loop
+        hidden
+      />
+    </div>
+  );
+};
 
-</div>
-
-  )
-}
-
-export default Header
+export default Header;
